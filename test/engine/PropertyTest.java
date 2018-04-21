4eefb784ab6d53d2864fef908e2f179db88ee97d
Louis-saglio <louis.saglio@sfr.fr>
2018-04-21 12:38:45 +0200

    [test] Add test on Property.filter method

---
+++ test/engine/PropertyTest.java
@@ -0,0 +1,31 @@
+package engine;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+public class PropertyTest {
+
+    @Test
+    public void testFilter() {
+        Property property = new Property("test");
+        ArrayList<Value> toRemain = new ArrayList<>();
+
+        Value toBeFiltered = new Value("value1");
+        property.add(toBeFiltered);
+
+        toRemain.add(new Value("value2"));
+        toRemain.add(new Value("value3"));
+
+        property.add(toRemain.get(0));
+        property.add(toRemain.get(1));
+
+        ArrayList<Value> filtered = property.filter(toBeFiltered);
+
+        for (int i = 0; i < toRemain.size(); i++) {
+            Assertions.assertEquals(toRemain.get(i).getId(), filtered.get(i).getId());
+        }
+    }
+
+}
