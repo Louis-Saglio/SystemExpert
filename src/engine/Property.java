58338fb09d736814eb224bdafc7b6f6bb6b3123e
Louis-saglio <louis.saglio@sfr.fr>
2018-04-21 12:37:33 +0200

    [add] Add constructor to Property and Value

--- src/engine/Property.java
+++ src/engine/Property.java
@@ -1,11 +1,18 @@
 package engine;
 
+import helpers.Helper;
+
 import java.util.ArrayList;
 
 public class Property extends ArrayList<Value> {
 
     private int id;
     private String name;
+
+    public Property(String name) {
+        this.id = Helper.getInstance().generateId();
+        this.name = name;
+    }
 
     public ArrayList<Value> filter(Value filteredValue) {
         ArrayList<Value> filteredList = new ArrayList<>();
--- src/engine/Value.java
+++ src/engine/Value.java
@@ -1,9 +1,16 @@
 package engine;
 
+import helpers.Helper;
+
 public class Value {
 
     private int id;
     private String name;
+
+    public Value(String name) {
+        this.id = Helper.getInstance().generateId();
+        this.name = name;
+    }
 
     public int getId() {
         return id;
