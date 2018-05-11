b0e8088ff1d9bfa9cadf2b05fa26bdf5706113da
Louis-Saglio <louis.saglio@sfr.fr>
2018-05-11 17:15:59 +0200

    [refactor] Split huge XMLReader.read() method into 3 smaller, safer, more optimized and more factorized methods

--- src/data_loader/XMLReader.java
+++ src/data_loader/XMLReader.java
@@ -1,64 +1,83 @@
 package data_loader;
 
 import engine.*;
-import org.jdom2.Document;
 import org.jdom2.Element;
 import org.jdom2.JDOMException;
 import org.jdom2.input.SAXBuilder;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
 public class XMLReader {
+    
+    private Rules rules;
+    private ArrayList<Proposition> truth;
+    private ArrayList<Proposition> error;
+    private Element root;
+    private HashMap<String, Property> properties;
+    private HashMap<String, Proposition> propositions;
 
-    public XMLReader() throws JDOMException, IOException {
-        this.read();
+    public XMLReader(String file) throws JDOMException, IOException {
+        this.root = new SAXBuilder().build(new File(file)).getRootElement();
+        this.getRules();
+        this.getTruth();
+        this.getError();
+        System.out.println(this.getTruth());
+        System.out.println(this.getError());
     }
 
-    public void read() throws JDOMException, IOException {
-        File dataFile = new File("data.xml");
-        SAXBuilder saxBuilder = new SAXBuilder();
-        Document document = saxBuilder.build(dataFile);
-        Element root = document.getRootElement();
-
-        HashMap<String, Property> properties = new HashMap<>();
-        List<Element> propertyElements = root.getChild("properties").getChildren();
-        for (Element propertyElement : propertyElements) {
-            Property property = new Property(propertyElement.getChild("name").getValue());
-            List<Element> valueElements = propertyElement.getChild("values").getChildren("value");
-            for (Element valueElement : valueElements) {
-                new Value(property, valueElement.getValue());
+    private HashMap<String, Property> getProperties() {
+        if (this.properties == null) {
+            this.properties = new HashMap<>();
+            List<Element> propertyElements = this.root.getChild("properties").getChildren();
+            for (Element propertyElement : propertyElements) {
+                Property property = new Property(propertyElement.getChild("name").getValue());
+                List<Element> valueElements = propertyElement.getChild("values").getChildren("value");
+                for (Element valueElement : valueElements) {
+                    new Value(property, valueElement.getValue());
+                }
+                this.properties.put(propertyElement.getAttributeValue("id"), property);
             }
-            properties.put(propertyElement.getAttributeValue("id"), property);
         }
+        return this.properties;
+    }
 
-        HashMap<String, Proposition> propositions = new HashMap<>();
-        List<Element> propositionElements = root.getChild("propositions").getChildren("proposition");
-        for (Element propositionElement : propositionElements) {
-            Element factElement = propositionElement.getChild("fact");
-            Property property = properties.get(factElement.getChild("property").getAttributeValue("ref"));
-            int valueIndex = Integer.parseInt(factElement.getChild("value").getAttributeValue("ref"));
-            Fact fact = new Fact(property, property.get(valueIndex));
-            Proposition proposition = new Proposition(fact, propositionElement.getChild("state").getValue().equals("true"));
-            propositions.put(propositionElement.getAttributeValue("id"), proposition);
-        }
-
-        Rules rules = new Rules();
-        List<Element> ruleElements = root.getChild("rules").getChildren("rule");
-        for (Element ruleElement : ruleElements) {
-            Rule rule = new Rule();
-            for (Element majorElement : ruleElement.getChild("majors").getChildren("major")) {
-                rule.getMajors().add(propositions.get(majorElement.getAttributeValue("ref")));
+    private HashMap<String, Proposition> getPropositions() {
+        if (this.propositions == null) {
+            this.propositions = new HashMap<>();
+            List<Element> propositionElements = this.root.getChild("propositions").getChildren("proposition");
+            for (Element propositionElement : propositionElements) {
+                Element factElement = propositionElement.getChild("fact");
+                Property property = this.getProperties().get(factElement.getChild("property").getAttributeValue("ref"));
+                int valueIndex = Integer.parseInt(factElement.getChild("value").getAttributeValue("ref"));
+                Fact fact = new Fact(property, property.get(valueIndex));
+                Proposition proposition = new Proposition(fact, propositionElement.getChild("state").getValue().equals("true"));
+                this.propositions.put(propositionElement.getAttributeValue("id"), proposition);
             }
-            for (Element conclusionElement : ruleElement.getChild("conclusions").getChildren("conclusion")) {
-                rule.getConclusions().add(propositions.get(conclusionElement.getAttributeValue("ref")));
-            }
-            rules.add(rule);
         }
-        System.out.println(rules);
+        return this.propositions;
+    }
 
+    public Rules getRules() {
+
+        if (this.rules == null) {
+            this.rules = new Rules();
+            List<Element> ruleElements = this.root.getChild("rules").getChildren("rule");
+            for (Element ruleElement : ruleElements) {
+                Rule rule = new Rule();
+                for (Element majorElement : ruleElement.getChild("majors").getChildren("major")) {
+                    rule.getMajors().add(this.getPropositions().get(majorElement.getAttributeValue("ref")));
+                }
+                for (Element conclusionElement : ruleElement.getChild("conclusions").getChildren("conclusion")) {
+                    rule.getConclusions().add(this.getPropositions().get(conclusionElement.getAttributeValue("ref")));
+                }
+                this.rules.add(rule);
+            }
+        }
+        return this.rules;
     }
 
 }
